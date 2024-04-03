3e94f6303ad96404e53b166b22e345541c87c6c0
Bryan Van paemel <bvan-pae@bess-f1r3s10.clusters.42paris.fr>
2024-04-03 16:52:33 +0200

    vFinal

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 09:27:44 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/04/03 10:53:13 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:05:37 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,30 @@
 #include "../source/minilibx/mlx_int.h"
 
 
+//--------------------STRUCT-----------------------
+
+typedef struct s_data
+{
+
+	char	**map;
+	char	**texture_paths;
+	size_t	colors[2];
+
+}				t_data;
+
+
 //---------------------PARSING---------------------
 
 int ft_reversecmp(char *str, char *cmp);
 int check_map(char *map);
-int display_error(char *error);
+int display_error(char *error, t_data *data);
+char	*ft_realloc(char *ptr, size_t size);
+void	get_raw_map(int map_fd, t_data* data);
+void free_tab(char **tab);
+void free_data(t_data *data);
+char **del_nfirst(int n, char **map);
+int	ft_tablen(char **tab);
+char **del_blank(char **map);
 
 //-------------------------------------------------
 
