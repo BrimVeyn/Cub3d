d93224810e979d37c454af45c0eaf74c2df9cc13
Bryan Van paemel <bvan-pae@bess-f1r2s10.clusters.42paris.fr>
2024-04-23 11:40:42 +0200

    vFinal

--- source/parsing/get_colors.c
+++ source/parsing/get_colors.c
@@ -3,22 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   get_colors.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nbardavi <nbabardavid@gmail.com>           +#+  +:+       +#+        */
+/*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:29:34 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/04/22 13:40:30 by nbardavi         ###   ########.fr       */
+/*   Updated: 2024/04/23 11:39:09 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-int	ft_colorcmp(char c)
+int	numbers_check(char *str)
 {
-	if (c == 'F')
-		return (C_F);
-	if (c == 'C')
-		return (C_C);
-	return (ERROR);
+	int	i;
+	int	num;
+
+	i = 0;
+	num = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]))
+			num++;
+		while (ft_isdigit(str[i]))
+			i++;
+		if (str[i])
+			i++;
+	}
+	return (num);
 }
 
 int	color_check(char *str)
@@ -28,10 +38,12 @@ int	color_check(char *str)
 
 	i = 0;
 	coma = 0;
+	if (numbers_check(str) != 3)
+		return (ERROR);
 	while (str[i])
 	{
-		if (!((str[i] >= '0' && str[i] <= '9') || str[i] == ','
-				|| ft_isws(str[i]) == TRUE))
+		if (((str[i] < '0' || str[i] > '9') && str[i] != ',')
+			&& ft_isws(str[i]) == ERROR)
 			return (ERROR);
 		if (str[i] == ',')
 			coma++;
@@ -49,12 +61,17 @@ int	get_componants(t_data *data, int i, int c, int idx)
 	int	b;
 
 	r = ft_atoi(&data->map[i][idx]);
-	idx += int_len(r) + 1;
 	idx += ws_count(&data->map[i][idx]);
+	idx += int_len(r);
+	idx += ws_count(&data->map[i][idx]);
+	idx++;
 	g = ft_atoi(&data->map[i][idx]);
-	idx += int_len(g) + 1;
+	idx += ws_count(&data->map[i][idx]);
+	idx += int_len(g);
+	idx += ws_count(&data->map[i][idx]);
+	idx++;
 	b = ft_atoi(&data->map[i][idx]);
-	data->colors[c] = (r << 16) + (g << 8) + (b << 1);
+	data->colors[c] = (r << 16) + (g << 8) + (b);
 	if ((r > 255 || r < 0) || (g > 255 || g < 0) || (b > 255 || b < 0))
 		return (ERROR);
 	return (TRUE);
@@ -66,9 +83,8 @@ int	get_colors_loop(t_data *data, int *off, int *idx, int *x)
 	*idx = ft_colorcmp(data->map[x[0]][*off]);
 	if (*idx != ERROR)
 	{
-		*off += ws_count(&data->map[x[0]][*off + 1]) + 1;
 		if (color_check(&data->map[x[0]][*off + 2]) == ERROR
-			|| get_componants(data, x[0], *idx, *off) == ERROR)
+			|| get_componants(data, x[0], *idx, *off + 1) == ERROR)
 			return (ERROR);
 		data->map = ft_delindex(data->map, x[0]);
 		if (!data->map)
